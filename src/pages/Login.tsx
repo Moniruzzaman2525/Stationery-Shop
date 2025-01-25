7dc1ccfc9d0f4e1f78df4be5eb9a2e2ff17c3a0d
Md Moniruzzaman <web.moniruzzaman1@gmail.com>
2025-01-25 11:51:47 +0600

    starting base api implement

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,23 +2,10 @@ import { Button, Row, Typography } from "antd";
 import SPForm from "../components/form/SPForm";
 import SPInput from "../components/form/SPInput";
 import { FieldValues } from "react-hook-form";
+import { formStyle, inputStyle } from "../styles/formStyles";
 
 const { Title } = Typography;
 
-const inputStyle = {
-    marginBottom: "5px",
-    padding: "10px",
-    borderRadius: "5px",
-    border: "1px solid #d9d9d9",
-}
-
-const formStyle = {
-    width: "400px",
-    padding: "30px",
-    borderRadius: "10px",
-    boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-    backgroundColor: "#fff",
-}
 
 const Login = () => {
     const onSubmit = (data: FieldValues) => {
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,23 +2,11 @@ import { Button, Row, Typography } from "antd";
 import SPForm from "../components/form/SPForm";
 import SPInput from "../components/form/SPInput";
 import { FieldValues } from "react-hook-form";
+import { formStyle, inputStyle } from "../styles/formStyles";
 
 const { Title, Text } = Typography;
 
-const inputStyle = {
-    marginBottom: "5px",
-    padding: "10px",
-    borderRadius: "5px",
-    border: "1px solid #d9d9d9",
-}
 
-const formStyle = {
-    width: "400px",
-    padding: "30px",
-    borderRadius: "10px",
-    boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-    backgroundColor: "#fff",
-}
 
 const Register = () => {
     const onSubmit = (data: FieldValues) => {
